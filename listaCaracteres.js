68e9178961ca1ceb511f2db59e239aa11c5b1992
Volpatto <93991362+MatheusVolp@users.noreply.github.com>
2025-06-02 19:26:09 -0300

    Create listaCaracteres.js

---
+++ listaCaracteres.js
@@ -0,0 +1,16 @@
+// Letras maiúsculas e minúsculas
+export const letras = [
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z'
+];
+
+// Números de 0 a 9
+export const numeros = ['0','1','2','3','4','5','6','7','8','9'];
+
+// Caracteres especiais comuns
+export const especiais = [
+  '!','@','#','$','%','^','&','*','(',')','-','_','=', '+',
+  '[',']','{','}','|',';',':',',','.','<','>','?','/','~','`'
+];
